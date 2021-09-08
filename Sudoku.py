a488c2f75d86215ac896b1eed20c534ef6cbd0e4
Colonel796 <53096134+Colonel796@users.noreply.github.com>
2021-09-08 16:12:13 -0500

    Add files via upload

---
+++ Sudoku.py
@@ -0,0 +1,23 @@
+class Sudoku:
+  def __init__(self, row, column, quad, number, possible):
+    self.row = row
+    self.column = column
+    self.quad = quad
+    self.number = number
+    self.possible = possible
+    
+def returnRow():
+    return row
+
+def reutrnColumn():
+    return colmn
+
+def returnQuad():
+    return quad
+
+def reutrnNumber():
+    return number 
+
+def returnPossible():
+    return possible
+
